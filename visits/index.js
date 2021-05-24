7985a876c1d95b0257b31c51f4688ba1a67572ef
Prasidh Aggarwal <prasaggarwal@deloitte.com>
2021-05-24 17:05:39 +0530

    working visits app

--- visits/index.js
+++ visits/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const redis = require('redis');
-const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -11,7 +10,6 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(0);
   client.get('visits', (err, visits) => {
     res.send('Number Of Visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
